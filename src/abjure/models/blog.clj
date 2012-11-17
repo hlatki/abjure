cad88ed58848905f48fd3e14a83fe9eec1ee983c
Justin Proffitt <justindproffitt@gmail.com>
2012-11-17 15:48:12 -0500

    Add functions to open markdown and json files.

--- src/abjure/models/blog.clj
+++ src/abjure/models/blog.clj
@@ -1,3 +1,9 @@
-(ns abjure.model.blog
+(ns abjure.models.blog
   (:require [markdown :as md]
             [cheshire.core :as json]))
+
+(defn get-md-file [filename]
+  (md/md-to-html-string (slurp filename)))
+
+(defn get-json-file [filename]
+  (json/parse-string (slurp filename) true))
