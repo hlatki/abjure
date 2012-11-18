a89dcb008e6205c224b5a9285b7e1706bf69b283
hlatki <hannahatkinson@gmail.com>
2012-11-18 12:46:53 -0500

    Added (finally) correct routing and templating for blog post. All of the logic is in the model.  The view just does routing and templating

--- src/abjure/models/blog.clj
+++ src/abjure/models/blog.clj
@@ -7,3 +7,14 @@
 
 (defn get-json-file [filename]
   (json/parse-string (slurp filename) true))
+
+;; this is for testing -- not sure what this should actually look like
+(def blog-path "/home/hannah/")
+
+
+(defn process-post-files [pname]
+  "Takes a name from the route and converts the associated files to a map"
+  (let [files (map #(str blog-path pname %) [".json" ".md"])
+        meta-data (get-json-file (first files))
+        content (get-md-file (second files))]
+    (assoc meta-data :content content)))
