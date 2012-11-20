6f18507f8a568f08fd84433ac71366d726b02b11
Justin Proffitt <justindproffitt@gmail.com>
2012-11-19 19:53:02 -0500

    Add abjure.models.blog/init-model.

--- src/abjure/models/blog.clj
+++ src/abjure/models/blog.clj
@@ -1,9 +1,31 @@
 (ns abjure.models.blog
   (:require [markdown :as md]
-            [cheshire.core :as json]))
+            [cheshire.core :as json])
+  (:use [clojure.java.io]))
+
+;; Utility for opening post related files.
 
 (defn get-md-file [filename]
   (md/md-to-html-string (slurp filename)))
 
 (defn get-json-file [filename]
   (json/parse-string (slurp filename) true))
+
+;; On startup of the server, build up our data-model
+
+(defn init-model [top-dir]
+  (let [post-dir (str top-dir "/posts/")
+        post-files (map #(.getName %) (.listFiles  (file post-dir)))
+        json-files (filter #(re-matches #".*\.json" %) post-files)
+        mapping {}]
+    (loop [j-files json-files json-map mapping]
+      (if (nil? (seq j-files))
+        json-map
+        (let [j-file (first j-files)
+              post-name (clojure.string/replace j-file #"\.json" "")]
+          (recur (rest j-files)
+                 (assoc json-map
+                    (keyword post-name)
+                    (assoc (get-json-file (str post-dir j-file))
+                           :name
+                           post-name))))))))
