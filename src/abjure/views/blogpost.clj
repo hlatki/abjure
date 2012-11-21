40aea53a84ef268c2ca9f4575aee8d5e163636c3
hlatki <hannahatkinson@gmail.com>
2012-11-20 22:34:43 -0500

    Route and templating for single post shouldd be working.  Note that I still need to do error handlng for this section.

--- src/abjure/views/blogpost.clj
+++ src/abjure/views/blogpost.clj
@@ -1,10 +1,19 @@
 (ns abjure.views.blogpost
-  (:use [noir.core]))
+  (:use [noir.core])
+  (:require [abjure.models.blog :as blog]
+            [abjure.views.common :as common]))
 
 ;; Page structure 
 
-(defpartial single-post [{:keys [title author date body]}]
-  ([:h1 title]
-   [:div.author "by " author]
-   [:div.datetime "on " date]
-   [:div.content body]))
+(defpartial single-post [{:keys [title author date content]}]
+            [:div 
+             [:h2 title]
+             [:span.author author]
+             [:span.date date]
+             [:p.blogpost content]])
+
+(defpage "/posts/:item" {:keys [item]}
+         (common/layout 
+           (single-post (blog/process-post-files item))))
+
+
